61f06f0c0220a6e6f2fb88d4dd82c6f3e2738ea3
henwil <tcheutchouawilfried70@gmail.com>
2026-01-04 17:36:37 +0100

    six

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,16 @@ router.get('/Parent', function(req, res, next) {
 router.get('/view_teacher', function(req, res, next) {
   res.render('view_teacher', { title: 'Proximity' });
 });
+router.get('/my_teacher', function(req, res, next) {
+  res.render('my_teacher', { title: 'informations' });
+});
+router.get('/my_request', function(req, res, next) {
+  res.render('my_request', { title: 'Request' });
+});
+router.get('/my_profil', function(req, res, next) {
+  res.render('my_profil', { title: 'Profil' });
+});
+router.get('/sign_in', function(req, res, next) {
+  res.render('sign_in', { title: 'sign_in' });
+});
 module.exports = router;
